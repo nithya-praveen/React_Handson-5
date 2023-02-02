dd640d7e5e73bf3ec483e9f26bcd923344fce10d
Nithya <111569028+nithya-praveen@users.noreply.github.com>
2023-02-02 23:51:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import ParentComp from './ParentComp/Parent';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComp/>
+    </div>
+  );
+}
+
+export default App;
